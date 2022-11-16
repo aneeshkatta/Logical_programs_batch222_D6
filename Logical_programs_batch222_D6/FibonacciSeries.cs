50d0a6ac585a757c02f1f807efd472f87113e194
Aneesh <katta.aneesh597@gmail.com>
2022-11-16 17:09:33 +0530

    added Q1_Fibonacci

--- Logical_programs_batch222_D6/FibonacciSeries.cs
+++ Logical_programs_batch222_D6/FibonacciSeries.cs
@@ -10,6 +10,21 @@ namespace Logical_programs_batch222_D6
     {
         public void FibonacciCheck()
         {
+            Console.WriteLine("Enter a number A to get Fibannoci series");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number B to get Fibannoci series");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a length of  Fibannoci series");
+            int Length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", A);
+            Console.WriteLine("{0}", B);
+            for (int i=1;i<= Length-2; i++)
+            {
+                int C = A + B;
+                Console.WriteLine("{0}", C);
+                A = B;
+                B = C;            
+            }
 
         }
     }
