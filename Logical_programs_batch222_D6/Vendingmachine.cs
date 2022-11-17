03ff4574450f67287b1a9bc96c2779ad1c60b125
Aneesh <katta.aneesh597@gmail.com>
2022-11-17 21:53:26 +0530

    added vending machine program

--- Logical_programs_batch222_D6/Vendingmachine.cs
+++ Logical_programs_batch222_D6/Vendingmachine.cs
@@ -10,7 +10,28 @@ namespace Logical_programs_batch222_D6
     {
         public void MinimumNotesCheck()
         {
+            Console.WriteLine("Enter an Amount to get Minimum Notes and its Values:");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[8] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int remainder =amount;
+            foreach (int note in array)
+            {
+                int i = 0;
+                while (remainder >= note)
+                {
+                    remainder = remainder - note;
+                    i++;
+                }
+                if (i > 0)
+                {
+                    Console.WriteLine("{0} X {1} = {2}", +note, +i, +(note * i));
+                }
+            }
+             
 
         }
+
     }
 }
+
+
