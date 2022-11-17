8cd4a214a13090fa316829812432dc410d86a04f
Aneesh <katta.aneesh597@gmail.com>
2022-11-17 13:39:32 +0530

    added Q3-PrimeNumberCheck

--- Logical_programs_batch222_D6/PrimeNumber.cs
+++ Logical_programs_batch222_D6/PrimeNumber.cs
@@ -10,6 +10,24 @@ namespace Logical_programs_batch222_D6
     {
         public void PrimeCheck()
         {
+            Console.WriteLine("Enter a number to check its a prime number or not!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0 && num!=0 && num!=1)
+                {
+                    a++;
+                }
+            }
+            if(a==1)
+            {
+                Console.WriteLine("{0} is Prime Number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Prime Number", num);
+            }
 
         }
     }
