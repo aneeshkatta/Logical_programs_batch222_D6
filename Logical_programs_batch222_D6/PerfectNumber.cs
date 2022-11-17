f95c5bf12a7aefe7d76f2c43ca25035c3aa8e1c6
Aneesh <katta.aneesh597@gmail.com>
2022-11-17 13:28:32 +0530

    added Q2-PerfectNumber

--- Logical_programs_batch222_D6/PerfectNumber.cs
+++ Logical_programs_batch222_D6/PerfectNumber.cs
@@ -10,7 +10,24 @@ namespace Logical_programs_batch222_D6
     {
         public  void PerfectCheck()
         {
-
+            Console.WriteLine("Enter a number to check its a perfect number or not!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i =1;i<num;i++)
+            {
+                if(num%i==0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if(sum==num)
+            {
+                Console.WriteLine("{0} is Perfect Number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Perfect Number", num);
+            }
         }
     }
 }
