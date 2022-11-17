a601ca7f106dd56d26d203d8e267615f64fae68f
Aneesh <katta.aneesh597@gmail.com>
2022-11-17 13:54:44 +0530

    added Q4-ReverseNumber

--- Logical_programs_batch222_D6/ReverseNumber.cs
+++ Logical_programs_batch222_D6/ReverseNumber.cs
@@ -10,7 +10,16 @@ namespace Logical_programs_batch222_D6
     {
         public void ReverseGenerate()
         {
-            
+            Console.WriteLine("Enter a No. to reverse");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;int remainder ;
+            while(number>0)
+            {
+                remainder = number % 10;
+                reverse = (reverse*10) + remainder;
+                number = number / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", +reverse);
         }
     }
 }
